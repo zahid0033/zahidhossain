286e677ade6da67afec0dfab3efcd20b47868101
zahid0033 <zahidhossain0033@gmail.com>
2024-06-03 11:14:13 +0600

    add: add logo to the title

--- .history/src/components/galleryModal_20240603104823.jsx
+++ .history/src/components/galleryModal_20240603104823.jsx
@@ -2,7 +2,7 @@ export default function GalleryModal({ gallery,onCancel}){
   return (
     <>
       <div className="backdrop" onClick={onCancel}></div>
-      <div className="grid lg:grid-cols-4 md:grid-cols-1 lg:w-3/5 md:w-11/12 sm:w-11/12 galleryModal">
+      <div className="grid lg:grid-cols-4 md:grid-cols-1 lg:w-3/5 md:w-11/12 galleryModal">
         <div className="lg:col-span-3">
           <img src={gallery.url} alt="" className="" />
         </div>
